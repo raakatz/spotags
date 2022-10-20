807ed01897790db4fb600c9279ac960ce4c400fa
Your Name <you@example.com>
2022-10-20 12:24:20 +0300

    structure

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ api_url = "https://api.spotify.com"
 redirect_uri = "https://localhost:8080/callback"
 scope = "user-library-read"
 
+def get_tokens():
+    global token, refresh_token
+    load_dotenv(f'{home_dir}/.spotag')
+    token = os.getenv('SPOTIFY_TOKEN')
+    refresh_token = os.getenv('SPOTIFY_REFRESH_TOKEN')
+
+
 @click.group()
 def spotags():
     """A CLI that manages Spotify album tags"""
@@ -28,37 +35,55 @@ def spotags():
 @spotags.command()
 def login():
     """Perform authorization"""
-    
+   
+    global token, refresh_token
     spotify = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
-    authorization_url, state = spotify.authorization_url(authorization_base_url)
-    
-    print('Please go here and authorize: ', authorization_url)
-    redirect_response = input('\n\nPaste the full redirect URL here: ')
-
     auth = HTTPBasicAuth(client_id, client_secret)
 
-    token = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)
-    with open(f'{home_dir}/.spotag', 'w') as f:
-        f.write(f'SPOTIFY_TOKEN={token["access_token"]}\n')
-        f.write(f'SPOTIFY_REFRESH_TOKEN={token["refresh_token"]}\n')
+    def update_spotag_file():
+        with open(f'{home_dir}/.spotag', 'w') as f:
+            f.write(f'SPOTIFY_TOKEN={token["access_token"]}\n')
+            f.write(f'SPOTIFY_REFRESH_TOKEN={token["refresh_token"]}\n')
 
+    def refresh():
+        r = spotify.refresh_token(token_url, refresh_token=refresh_token, auth=auth)
+        update_spotag_file()
+    
+    def authorize():
+        authorization_url, state = spotify.authorization_url(authorization_base_url) 
+        print('Please go here and authorize: ', authorization_url)
+        redirect_response = input('\n\nPaste the full redirect URL here: ')
+        r = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)
+        update_spotag_file()
+    
+    if os.path.exists(f'{home_dir}/.spotag'):
+        refresh()
+    else:
+        authorize()
+    
 
 @spotags.command()
-def albums():
+def pull():
     """List albums"""
 
-    load_dotenv(f'{home_dir}/.spotag')
-    token = os.getenv('SPOTIFY_TOKEN')
-    headers = {
-            "Content-Type": "application/json",
-            "Authorization": f'Bearer {token}'
-            }
-    params = {
-            "limit": 2
-            }
-    r = requests.get(f'{api_url}/v1/me/albums', headers=headers, params=params)
+    load_tokens()
+    
+    offset = 0
 
-    print(r.text)
+    while True:
+        headers = {
+                "Content-Type": "application/json",
+                "Authorization": f'Bearer {token}'
+                }
+        params = {
+                "limit": 1,
+                "offset": offset
+                }
+        r = requests.get(f'{api_url}/v1/me/albums', headers=headers, params=params)
+        if r.status_code != 200:
+            print(f'Could not fetch albums:\n{r.text}')
+            return False
+
 
 
 if __name__ == '__main__':
